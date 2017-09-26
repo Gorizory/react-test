b09d9fcd0fde732c8c574976f40900d4e76623ba
Gorizory <sobenin.g@gmail.com>
2017-09-26 12:23:55 +0300

    Master

--- src/App.js
+++ src/App.js
@@ -1,32 +1,9 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      number : 0,
-    }
-  }
-
-  plus() {
-    this.setState({
-      number : this.state.number + 1,
-    });
-  }
-
-  minus() {
-    this.setState({
-      number : this.state.number - 1,
-    });
-  }
-
   render() {
     return (
-      <div>
-        <div> {this.state.number} </div>
-        <button onClick={this.plus.bind(this)}> Plus </button>
-        <button onClick={this.minus.bind(this)}> Minus </button>
-      </div>
+      <h1>Hello World!</h1>
     );
   }
 }
